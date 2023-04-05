4da054133419be8a71e048b3fc6cfc38f4ffe4a8
Mikosy <72047880+Mikosy@users.noreply.github.com>
2023-04-05 02:07:43 +0100

    Add files via upload

---
+++ nav/js/script.js
@@ -0,0 +1,8 @@
+console.log("Kunichiwa Mf")
+
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
